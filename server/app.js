1f76b16349d09420f8ae8a8f9d71988abff86f62
averywlittle <averywlittle@gmail.com>
2020-12-17 23:32:13 -0500

    prod build

--- server/app.js
+++ server/app.js
@@ -63,7 +63,7 @@ app.post('/api/query/', (request, response, next) => {
 })
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
